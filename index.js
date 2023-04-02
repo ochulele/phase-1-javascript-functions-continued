9d4e08dbf8b8828b7abeac60a67a4a32f9806ed4
ochuk <ochuciano@gmail.com>
2023-04-02 03:51:53 -0400

    javascript functions

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+function saturdayFun(action="roller-skate") {
+    return (`This Saturday, I want to ${action}!`);
+}
+saturdayFun();
+
+function mondayWork(work = "go to the office") {
+    return `This Monday, I will ${work}.`;
+}
+mondayWork();
+
+function wrapAdjective(system = "*") {
+    return function(number1 = "special") {
+        return `You are ${system}${number1}${system}!`
+    }
+}
+
